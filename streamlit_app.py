82e3e9a2636ae0c968b54117405f7d84f01fd1a8
sirisha <109633616+a-siri@users.noreply.github.com>
2022-07-20 10:50:43 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,6 @@ st.title("my name is sirisha")
 
 st.text("my hub kalyan")
 st.text("my kid ratnika")
-import pandas as pd
 
-df = pd.DataFrame(
-    
-    columns=(("my hub","kalyan"),("my kid","ratnika")))
 
-st.table(df)
+st.table(data=None)
