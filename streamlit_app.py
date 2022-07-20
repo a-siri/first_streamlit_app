f41fefd1df1187ce2434f79561b5126309000091
sirisha <109633616+a-siri@users.noreply.github.com>
2022-07-20 10:43:27 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.text("my kid ratnika")
 import pandas as pd
 import numpy as np
 df = pd.DataFrame(
-    np.random.randn(10, 5),
+    np.random.randn(2, 2),
     columns=('col %d' % i for i in range(5)))
 
 st.table(df)
