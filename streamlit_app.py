e88e7060e1f185ab90861cb5557689a637727d21
sirisha <109633616+a-siri@users.noreply.github.com>
2022-07-20 10:41:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ st.title("my name is sirisha")
 st.text("my hub kalyan")
 st.text("my kid ratnika")
 import pandas as pd
+import numpy as np
 df = pd.DataFrame(
     np.random.randn(10, 5),
     columns=('col %d' % i for i in range(5)))
