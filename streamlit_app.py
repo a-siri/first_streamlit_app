6a20f193793d8dd39c8939de66709c6fded74725
sirisha <109633616+a-siri@users.noreply.github.com>
2022-07-20 11:00:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,29 @@ st.text("my kid ratnika")
 import pandas as pd
 import numpy as np
 df = pd.DataFrame(
-    np.random(2, 2),
+    np.random.randn(2, 2),
     columns=((1,"my hub","kalyan"),(2,"my kid","ratnika")))
 
 st.table(df)
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(
+    np.random.randn(10, 5),
+    columns=("col %d" % i for i in range(5)))
+
+# CSS to inject contained in a string
+hide_table_row_index = """
+            <style>
+            tbody th {display:none}
+            .blank {display:none}
+            </style>
+            """
+
+# Inject CSS with Markdown
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
+# Display a static table
+st.table(df)
+
