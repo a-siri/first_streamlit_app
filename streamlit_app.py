1bdab90472bde5d11dd845ba0ac1f1bec2f6c9f4
sirisha <109633616+a-siri@users.noreply.github.com>
2022-07-20 10:48:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("my name is sirisha")
 st.text("my hub kalyan")
 st.text("my kid ratnika")
 import pandas as pd
-import numpy as np
+
 df = pd.DataFrame(
     
     columns=(("my hub","kalyan"),("my kid","ratnika")))
