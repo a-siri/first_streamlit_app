d538daa1e45566cb12e5152a48fe0578e809e609
sirisha <109633616+a-siri@users.noreply.github.com>
2022-07-20 11:17:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
-import streamlit 
-streamlit.title('my parents healthy dinner')
+import streamlit as st
+st.title('my parents healthy dinner')
+st.title("my name is sirisha")
+
+st.text("my hub kalyan")
+st.text("my kid ratnika")
